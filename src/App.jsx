e1b486785a11770141475ce13b05b92520600c2d
Ethan <sangnguyen07dhth@gmail.com>
2025-07-20 21:11:15 +0700

    Add header and code markup of featuremovies component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Movie App</h1>
-    </div>
+    <>
+      <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
+        <nav className="flex items-center gap-4">
+          <img src="./netflix.png" alt="Logo" className="w-16 sm:w-28" />
+          <a href="#">Phim</a>
+          <a href="#">Truyền hình</a>
+        </nav>
+        <div>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+      <main>
+        <section className="feature-movies">
+          <img src="https://image.tmdb.org/t/p/original/xg27NrXi7VXCGUr7MG75UqLl6Vg.jpg" alt="Movie Background" />
+          <div>
+            <p>Inside Out 2</p>
+            <div>
+              <p>PG13</p>
+              <p>2025-06-11</p>
+            </div>
+            <article>
+              <h3>Overview</h3>
+              <p>
+                Teenager Riley&apos;s mind headquarters is undergoing a sudden
+                demolition to make room for something entirely unexpected: new
+                Emotions! Joy, Sadness, Anger, Fear and Disgust, who&apos;ve long been
+                running a successful operation by all accounts, aren&apos;t sure how to
+                feel when Anxiety shows up. And it looks like she&apos;s not alone.
+              </p>
+            </article>
+            <div>
+              <button>
+                <FontAwesomeIcon icon={faPlay}/>
+                Trailer
+              </button>
+              <button>
+                View Detail
+              </button>
+            </div>
+          </div>
+          <div>
+            <ul>
+              <li className="w-4 h-0.5 bg-slate-600 cursor-pointer"></li>
+              <li className="w-4 h-0.5 bg-slate-600 cursor-pointer"></li>
+              <li className="w-4 h-0.5 bg-slate-600 cursor-pointer"></li>
+              <li className="w-4 h-0.5 bg-slate-600 cursor-pointer"></li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
