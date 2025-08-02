10af60f78cdb541c5f54562c1ed06c23c8f7e3fd
Ethan <sangnguyen07dhth@gmail.com>
2025-08-02 13:41:55 +0700

    Add auto slide

--- src/components/FeatureMovies/PaginateIndicator.jsx
+++ src/components/FeatureMovies/PaginateIndicator.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const PaginateIndicator = (props) => {
-  const {movies, activeMovieId, setActiveMovieId} = props;
+  const { movies, slideMovieIndex, setSlideMovieIndex } = props;
 
   return (
     <div className="absolute right-8 bottom-[10%]">
       <ul className="flex gap-1">
-        {
-          movies.map((movie) => {
-            return <li 
-            key={movie.id} 
-            className={`h-1 w-6 cursor-pointer bg-slate-${movie.id === activeMovieId ? '100' : '600'}`}
-            onClick={() => setActiveMovieId(movie.id) }></li>
-          })
-        }
+        {movies.map((movie, index) => {
+          return (
+            <li
+              key={movie.id}
+              className={`h-1 w-6 cursor-pointer ${index === slideMovieIndex ? 'bg-slate-100' : 'bg-slate-500'}`}
+              onClick={() => setSlideMovieIndex(index)}
+            ></li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -1,42 +1,67 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-import PaginateIndicator from "./PaginateIndicator";
+import { useEffect, useState } from 'react';
+import Movie from './Movie';
+import PaginateIndicator from './PaginateIndicator';
 
 const FeatureMovies = () => {
   const [movies, setMovies] = useState([]);
-  const [activeMovieId, setActiveMovieId] = useState();
+  // const [activeMovieId, setActiveMovieId] = useState();
+  const [slideMovieIndex, setSlideMovieIndex] = useState(0);
 
   useEffect(() => {
     const options = {
-      method: "GET",
+      method: 'GET',
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization:
-          "Bearer XXX",
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTgyYjQyYzMyZTExMDMyYTAyMmY3OTYzYzljNzIyMSIsIm5iZiI6MTc1MzYwNTI0My44MDMsInN1YiI6IjY4ODVlNDdiNmMwZGYzZGE5YjE2YzFkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pg2d9e2VMploGY60BUZFaEj3ueOFk1yuarjuf3JLyZc',
       },
     };
 
     fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-      options,
+      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+      options
     )
       .then((res) => res.json())
       .then((res) => {
         const popularMovies = res.results.slice(0, 4);
         setMovies(popularMovies);
-
-        if (popularMovies[0]) setActiveMovieId(popularMovies[0].id);
       })
       .catch((err) => console.error(err));
   }, []);
-  console.log(movies);
 
-  const activeMovie = movies.find((movie) => movie.id === activeMovieId);
+  // Auto-slide effect
+  useEffect(() => {
+    if (movies.length === 0) return;
+
+    console.log('affect');
+
+    const interval = setInterval(() => {
+      setSlideMovieIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % movies.length;
+        return nextIndex;
+      });
+    }, 5000); // Change slide every 3 seconds
+
+    return () => {
+      console.log('clear');
+      clearInterval(interval);
+    };
+  }, [movies]);
+
+  console.log('movies', movies);
 
   return (
     <section className="feature-movies relative text-white">
-      {activeMovie && <Movie data={activeMovie} />}
-      <PaginateIndicator movies={movies} activeMovieId={activeMovieId} setActiveMovieId={setActiveMovieId}/>
+      {movies[slideMovieIndex] && (
+        <>
+          <Movie data={movies[slideMovieIndex]} />
+          <PaginateIndicator
+            movies={movies}
+            slideMovieIndex={slideMovieIndex}
+            setSlideMovieIndex={setSlideMovieIndex}
+          />
+        </>
+      )}
     </section>
   );
 };
