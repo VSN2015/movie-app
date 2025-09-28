db23628891f44edcef1ae184a76a32457f3cb2aa
Ethan <sangnguyen07dhth@gmail.com>
2025-09-28 21:38:05 +0700

    split banner to another component and reuse it

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,10 +1,9 @@
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CircularProgressBar from '../components/CircularProgressBar';
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
-import { groupBy } from 'lodash';
 import Loading from '../components/Loading';
+import Banner from '../components/MediaDetail/Banner';
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -34,75 +33,12 @@ const MovieDetail = () => {
       })
   }, [id])
 
-  const releaseDateResults = movieInfo.release_dates?.results || []
-  const releaseDates = releaseDateResults.find(result => result.iso_3166_1 == 'US')?.release_dates || []
-  const certificationLabel = releaseDates.find(releaseDate => releaseDate.certification)?.certification
-  const genres = (movieInfo.genres || []).map(genre => genre.name).join(', ')
-
-  const crews = (movieInfo.credits?.crew || []).filter(crew => ['Director', 'Screenplay', 'Writer'].includes(crew.job))
-  const crewInfo = crews.map(crew => ({id: crew.id, job: crew.job, name: crew.name}))
-  const groupedCrews = groupBy(crewInfo, 'job')
-  const groupedCrewKeys = Object.keys(groupedCrews)
-  const voteAverage = movieInfo.vote_average || 0
-  console.log(groupedCrews)
-
   if(isLoading){
     return <Loading/>
   }
 
   return (
-    <section
-      className="movie-detail bg-black/70 bg-cover bg-center [background-blend-mode:multiply]"
-      style={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movieInfo.backdrop_path})`
-      }}
-    >
-      <div className="mx-auto flex sm:max-w-3xl lg:max-w-full gap-6 p-6">
-        <div className="flex-1/3">
-          <img
-            src={`https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movieInfo.poster_path}`}
-            alt=""
-          />
-        </div>
-        <div className="flex-2/3 text-[1.2vw] text-white">
-          <h2 className="mb-2 text-[2vw] font-bold">{movieInfo.title}</h2>
-          <div className="flex items-center gap-4">
-            <span className="border border-gray-400 p-1 text-gray-400">{certificationLabel}</span>
-            <p>{movieInfo.release_date}</p>
-            <p>{genres}</p>
-          </div>
-          <div className="mt-4 flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <CircularProgressBar percent={Math.round(voteAverage * 10)} size={3.5} strokeWidth={0.3} />{' '}
-              Rating
-            </div>
-            <button className='cursor-pointer'>
-              <FontAwesomeIcon icon={faPlay} className="mr-[.7]" /> Trailer
-            </button>
-          </div>
-          <div className='mt-4'>
-            <h3 className='font-bold text-[1.3vw] mb-2'>Overview</h3>
-            <article>
-              {movieInfo.overview}
-            </article>
-          </div>
-          <div className={`mt-4 grid grid-cols-${groupedCrewKeys.length} gap-2`}>
-            {
-              groupedCrewKeys.map(job => (
-                <div key={job}>
-                  <p className='font-bold'>{job}</p>
-                  <p>
-                    {
-                      groupedCrews[job].map(crew => crew.name).join(', ')
-                    }
-                  </p>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      </div>
-    </section>
+    <Banner mediaInfo={movieInfo}/>
   );
 };
 
